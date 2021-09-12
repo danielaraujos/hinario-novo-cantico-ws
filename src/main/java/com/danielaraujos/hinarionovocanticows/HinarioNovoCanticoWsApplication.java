4c2483498c05c9fcbd14416643d7be9b1d0ef3db
Daniel Araujo <danielaraujos@live.com>
2021-09-12 14:50:49 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/danielaraujos/hinarionovocanticows/HinarioNovoCanticoWsApplication.java
@@ -0,0 +1,13 @@
+package com.danielaraujos.hinarionovocanticows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinarioNovoCanticoWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HinarioNovoCanticoWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danielaraujos/hinarionovocanticows/HinarioNovoCanticoWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielaraujos.hinarionovocanticows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HinarioNovoCanticoWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
