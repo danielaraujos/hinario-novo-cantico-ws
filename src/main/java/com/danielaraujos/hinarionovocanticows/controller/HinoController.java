786c289d0490bad63efc00961720241d062920d8
Daniel Araujo <danielaraujos@live.com>
2021-09-13 21:35:17 -0300

    :sparkles: implementa método para insert

--- src/main/java/com/danielaraujos/hinarionovocanticows/controller/HinoController.java
+++ src/main/java/com/danielaraujos/hinarionovocanticows/controller/HinoController.java
@@ -1,15 +1,22 @@
 package com.danielaraujos.hinarionovocanticows.controller;
 
 import com.danielaraujos.hinarionovocanticows.controller.dto.HinoDto;
+import com.danielaraujos.hinarionovocanticows.controller.form.HinoForm;
 import com.danielaraujos.hinarionovocanticows.model.Hino;
 import com.danielaraujos.hinarionovocanticows.repository.HinoRepository;
+import com.danielaraujos.hinarionovocanticows.repository.IndiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,6 +25,9 @@ public class HinoController {
 
     @Autowired
     private HinoRepository hinoRepository;
+
+    @Autowired
+    private IndiceRepository indiceRepository;
 
     @GetMapping
     public Page<HinoDto> listarComPaginacao(@PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 30)
@@ -30,6 +40,18 @@ public class HinoController {
     public List<HinoDto> listarSemPaginacao() {
         List<Hino> hinos = hinoRepository.findAll();
         return HinoDto.converterLista(hinos);
+    }
+
+    @PostMapping
+    public ResponseEntity<HinoDto> cadastrar(@RequestBody @Valid HinoForm form, UriComponentsBuilder uriBuilder) {
+        try {
+            Hino hino = form.converter(indiceRepository);
+            hinoRepository.save(hino);
+            URI uri = uriBuilder.path("/hinos/{id}").buildAndExpand(hino.getId()).toUri();
+            return ResponseEntity.created(uri).body(new HinoDto(hino));
+        } catch (SecurityException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
 
