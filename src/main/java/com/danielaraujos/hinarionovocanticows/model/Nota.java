e31f3b2a0597405c0032dfb40105d6dd3f44fa0f
Daniel Araujo <danielaraujos@live.com>
2021-09-15 11:18:54 -0300

    :sparkles: cria modelo para nota musical

---
+++ src/main/java/com/danielaraujos/hinarionovocanticows/model/Nota.java
@@ -0,0 +1,30 @@
+package com.danielaraujos.hinarionovocanticows.model;
+
+import javax.persistence.*;
+
+@Entity(name = "TSANOTA")
+public class Nota {
+
+    @Id
+    @Column(name = "nota", updatable = false, unique = true, nullable = false)
+    private String nota;
+
+
+    public Nota() {
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Nota{" +
+                "nota='" + nota + '\'' +
+                '}';
+    }
+}
