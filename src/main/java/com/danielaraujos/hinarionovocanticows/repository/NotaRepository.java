eb945d76a072cc368b644d0448bb9313d1cf47cb
Daniel Araujo <danielaraujos@live.com>
2021-09-15 11:20:07 -0300

    :sparkles: cria nota repository

---
+++ src/main/java/com/danielaraujos/hinarionovocanticows/repository/NotaRepository.java
@@ -0,0 +1,9 @@
+package com.danielaraujos.hinarionovocanticows.repository;
+
+import com.danielaraujos.hinarionovocanticows.model.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface NotaRepository extends JpaRepository<Nota, String> {
+
+}
